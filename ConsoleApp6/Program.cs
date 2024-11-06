d2e48006bf1cca83c35b32454ba4e242e60923ff
Makhsum <dd4006189@gmail.com>
2024-11-06 16:39:33 +0100

    initial commit

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using System.IO.Compression;
+using Microsoft.Web.Administration;
+
+var vaersionInfo = FileVersionInfo.GetVersionInfo("C:\\Users\\MakhsumYusupov\\Downloads\\PeakboardHubOnline(8)\\Peakboard.HUB.Cloud.exe");
+string filePath = Path.Combine(Path.GetTempPath(), "PeakboardHubOnline.zip");
+string url = "https://downloads.peakboard.com/download/PeakboardHubOnline/dev_master/PeakboardHubOnline.zip";
+HttpClient client = new HttpClient();
+var res = client.GetAsync("https://peakboardpipelines.azurewebsites.net/products/PeakboardHubOnline/dev_master/version").Result;
+var responceBody = res.Content.ReadAsStringAsync().Result;
+//byte[] fileBytes = await client.GetByteArrayAsync(url);
+//await File.WriteAllBytesAsync(filePath, fileBytes);
+// EndpackFiles();
+using (ServerManager serverManager = new ServerManager())
+{
+    foreach (var site in serverManager.Sites)
+    {
+        Console.WriteLine(site.Name);
+    }
+}
+Console.WriteLine(vaersionInfo.FileVersion);
+
+
+Console.ReadLine();
+
+void EndpackFiles()
+{
+    string extractPath = "C:\\Users\\MakhsumYusupov\\Desktop\\test1";
+    DirectoryInfo dir = new DirectoryInfo(extractPath);
+    string zipData = filePath;
+    foreach (FileInfo item in dir.GetFiles())
+    {
+        item.Delete();
+    }
+
+    foreach (DirectoryInfo directory in dir.GetDirectories())
+    {
+        directory.Delete(true);
+    }
+
+    ZipFile.ExtractToDirectory(zipData,extractPath);
+}
